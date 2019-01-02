5097f522c0cc6c59b5521b434eedecb82ea923c8
monkeyChen <livepower@gmail.com>
2019-01-03 00:42:30 +0800

    modified path

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  baseUrl: '/my-vue-cli3/dist/',
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+}
