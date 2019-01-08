fc55be77a6e02bbacaf35d74d09a97ea816ee324
monkeyChen <livepower@gmail.com>
2019-01-08 16:04:12 +0800

    add bootstrap

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-// import 'bootstrap'
+import 'bootstrap'
 import Loading from 'vue-loading-overlay'
 import 'vue-loading-overlay/dist/vue-loading.css'
 import './bus';
